f86cfcfa0ca9b1a3db4e52f04fd39b668fbaaeb7
Nicolas320 <felipenicolas245@gmail.com>
2023-11-05 19:12:29 -0500

    cambios

--- resources/views/contenido/principal.blade.php
+++ resources/views/contenido/principal.blade.php
@@ -4,8 +4,10 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="{{ asset('css/Principal.css') }}" />
+   <link rel="shortcut icon" href="img/logoac.png" type="image/x-icon">
 
-   <title>Document</title>
+
+   <title>Transport_GPS</title>
 </head>
 <body>
 <header class="cabeza" id="inicio">
